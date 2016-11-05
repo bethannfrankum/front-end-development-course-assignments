4d73b1286f86575211fc5b62ea2c3c92426ce858
bethannfrankum <bethannfrankum88@gmail.com>
2016-11-05 13:04:08 -0500

    Assignment 10

--- Assignment10/JS-Assignment/scripts/main.js
+++ Assignment10/JS-Assignment/scripts/main.js
@@ -5,10 +5,24 @@ function update() {
     ///Part 1
     var firstName = getValue('firstName');
     var lastName = getValue('lastName');
+    var fullName = firstName + " " + lastName;
+    if (firstName != "" && lastName != "") {
+        document.getElementById("p1_valid").innerHTML = true;
+
+    } else {
+        document.getElementById("p1_valid").innerHTML =
+            false;
+
+    }
+
+    var fullNameLength = firstName.length + lastName.length;
+
+    document.getElementById("p1_fullNameLength").innerHTML = fullNameLength;
     console.log(firstName);
+
     console.log(lastName);
 
-
+    document.getElementById("p1_fullName").innerHTML = fullName
 
 
     // Part 2
@@ -16,18 +30,47 @@ function update() {
     console.log(emailAddress);
 
 
+    {
+        document.getElementById("p2_email").innerHTML = emailAddress;
+    }
+    if (emailAddress.indexOf('@') > -1) {
+        document.getElementById("p2_valid").innerHTML = true;
+    } else {
+        document.getElementById("p2_valid").innerHTML = false;
+    }
+
+
+    document.getElementById("p2_emailLength").innerHTML = emailAddress.length;
+
+
     // Part 3
     var randomNumber = getValue('randomNumber');
     console.log(randomNumber);
+
+
+    {
+        document.getElementById("p3_number").innerHTML =
+            randomNumber;
+    }
+    if (randomNumber >= 0) {
+        document.getElementById("p3_valid").innerHTML = true;
+    } else {
+        document.getElementById("p3_valid").innerHTML = false;
+    }
 
 
 
 
     // Part 4
     var arrayInput = getValue('arrayValue');
+
+    {
+        document.getElementById("p4_valid").innerHTML = arrayInput;
+    } {
+        document.getElementById("p4_arraylength").innerHTML = arrayInput.length;
+    }
+
     console.log(arrayInput);
-
-
 
 
 
