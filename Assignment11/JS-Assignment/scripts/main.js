6d4e0b0d7fbf0e3f8f51aafd727f970de7574951
bethannfrankum <bethannfrankum88@gmail.com>
2016-11-09 08:45:54 -0600

    assignment 10 and 11 progress
    css still needs work and questions are not answered on 11

---
+++ Assignment11/JS-Assignment/scripts/main.js
@@ -0,0 +1,58 @@
+function update() {
+
+
+
+    //////////////////////My Code/////////////////////
+
+
+    var firstName = getValue('firstName');
+    var lastName = getValue('lastName');
+    var emailAddress = getValue('emailAddress')
+
+
+    var messages = []
+
+    if (firstName == '') {
+        messages.push("First name required")
+    }
+
+    if (lastName == '') {
+        messages.push("Last name is required")
+    }
+    if (emailAddress == '') {
+        messages.push("Email address is required")
+    }
+    var html = '<ul>'
+
+    for (var i = 0; i < messages.length; i++) {
+        html = html + '<li>' + messages[i] + '</li>'
+    }
+
+    html = html + '</ul>'
+
+    document.getElementById('error-list').innerHTML = html;
+    var user = {
+        firstname: firstName,
+        lastname: lastName,
+        email: emailAddress,
+        fullname: function () {
+            return this.firstName + " " + this.lastName;
+        }
+    }
+
+    if (firstName != "" && lastName != "" && emailAddress != "") {
+        window.alert("Thank your for completing this form!")
+    }
+}
+
+////
+var btn = document.getElementById('btnUpdate');
+btn.addEventListener('click', update);
+
+function getValue(id) {
+    return getElm(id).value;
+}
+
+function getElm(id) {
+    return document.getElementById(id);
+}
