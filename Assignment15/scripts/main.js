0bef09f1d0a095ab18b324c67ecde72628d8efe0
bethannfrankum <bethannfrankum88@gmail.com>
2016-12-01 17:25:49 -0600

    assignment 15

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -1,10 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    ///// Code goes here
+    ////// Part 1
+    $('#btnOne').on('click', update1)
 
-     ///// Code goes here
+    function update1() {
+        return alert('Hello');
+    }
+    /////Part 2
+    $('#btnTwo').on('click', update2)
 
+    function update2() {
+        $('.update-html').html('<h3>Hello World</h3>')
+    }
+    ////Part 3
+    $('#btnThree').on('click', update3)
 
+    function update3() {
+        $('#text-update').val('Hello World')
+    }
+    /////Part 4
+    $('#btnCopyHtml').on('click', update4)
 
+    function update4() {
+        var copiedHtml = $('.copy-html').html();
+        $('.paste-html').html(copiedHtml);
+    }
+    ////Part 5
+    $('#btnCopyValues').on('click', update5)
 
-
-
+    function update5() {
+        var copiedValue = $('#copy-text').val();
+        $('#paste-text').val(copiedValue);
+    }
 });
