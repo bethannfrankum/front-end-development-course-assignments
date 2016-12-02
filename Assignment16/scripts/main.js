d3e90185427b679daf952ef6b0c6e0c0c16ecb38
bethannfrankum <bethannfrankum88@gmail.com>
2016-12-01 19:16:49 -0600

    Completed Assignment 16

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,5 +1,32 @@
-$(document).ready(function(){
-
-
-
+$(document).ready(function () {
+    //Part2//
+    $('#btnShow').on('click', function () {
+        $('.display-basic').show();
+    });
+    $('#btnHide').on('click', function () {
+        $('.display-basic').hide();
+    });
+    $('#btnToggle').on('click', function () {
+        $('.display-basic').toggle();
+    });
+    //Part4///
+    $('#btnFadeIn').on('click', function () {
+        $('.display-fade').fadeIn('quick');
+    });
+    $('#btnFadeOut').on('click', function () {
+        $('.display-fade').fadeOut('slow');
+    });
+    $('#btnFadeToggle').on('click', function () {
+        $('.display-fade').fadeToggle();
+    });
+    //Part5//
+    $('#btnSlideDown').on('click', function () {
+        $('.display-slide').slideDown(1000);
+    });
+    $('#btnSlideUp').on('click', function () {
+        $('.display-slide').slideUp(500);
+    });
+    $('#btnSlideToggle').on('click', function () {
+        $('.display-slide').slideToggle();
+    });
 });
