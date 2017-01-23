3265bc4c1f9c9dc6e32646dd513816dc8d7fe269
bethannfrankum <bethannfrankum88@gmail.com>
2017-01-23 10:50:06 -0600

    drop down menus sorta work except you have to click the menu itself in order for the menu to shrink back up, clicking a menu item does nothing and lets the menus remain on the screen, even after you click on the second drop down menu  for custom orders

--- Assignment24/bethannframeworks/scripts/main.js
+++ Assignment24/bethannframeworks/scripts/main.js
@@ -11,4 +11,10 @@ $(document).ready(function () {
         var navi = btn.closest('.row');
         navi.find('.drop-down-content').slideToggle();
     });
+    $('.drop-down-toggle2').on('click', function (e) {
+        e.preventDefault();
+        var btn = $(this);
+        var navi = btn.closest('.row');
+        navi.find('.drop-down-content2').slideToggle();
+    });
 });
