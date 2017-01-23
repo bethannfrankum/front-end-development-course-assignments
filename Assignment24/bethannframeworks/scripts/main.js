a5d93cbb2284c416caaf79af9eab58b8709b674d
bethannfrankum <bethannfrankum88@gmail.com>
2017-01-23 10:32:59 -0600

    toggle for drop down menus almost fixed

---
+++ Assignment24/bethannframeworks/scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.mobile-collapse').hide();
+    $('.mobile-collapse-toggle').on('click', function () {
+        var btn = $(this);
+        var navi = btn.closest('.row');
+        navi.find('.mobile-collapse').slideToggle();
+    });
+    $('.drop-down-toggle').on('click', function (e) {
+        e.preventDefault();
+        var btn = $(this);
+        var navi = btn.closest('.row');
+        navi.find('.drop-down-content').slideToggle();
+    });
+});
